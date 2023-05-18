168dc504b745900ba5154c37d9ef81d051d735ef
heshica-vp <heshica.vanapalli21@st.niituniversity.in>
2023-05-18 15:10:28 +0530

    First Commit

---
+++ CarLockApp/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import AppNav from './src/navigation/AppNav';
+import { AuthProvider } from './src/context/AuthContext';
+
+function App() {
+  return (
+    <AuthProvider>
+      <AppNav></AppNav>
+    </AuthProvider>
+  );
+}
+
+export default App;
